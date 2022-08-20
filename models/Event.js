2cc1142d9a1826691c376dea7a653db2db8aba04
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-08-20 13:56:03 +0200

    Create Event Model

---
+++ models/Event.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, 'Please provide a first name']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Please provide a last name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide an email'],
+        unique: true,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please provide a valid email"
+        ],
+    },
+    date: {
+        type: Date,
+        required: [true, 'Please provide a date'],
+    },
+})
