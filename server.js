6c13e58707ea5bb03def01bbde68eee1d70ba807
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-08-21 16:05:02 +0200

    Add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config({path: "./config.env"});
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 // Connect DB
 connectDB();
@@ -8,6 +9,7 @@ connectDB();
 const app = express();
 
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 app.use('/api/events', require('./routes/events'));
