471fb6deef7e08e73a30aed1264e28cb4b98528d
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-08-23 13:49:51 +0200

    Refactor postEvents

--- controllers/events.js
+++ controllers/events.js
@@ -6,7 +6,8 @@ exports.getEvents = async (req, res, next) => {
         res.json({message: 'OK', data: events});
       }
       catch(err) {
-        res.status(500).json({ message: err });
+        res.status(500);
+        res.json({ message: err });
       }
   }
 
@@ -17,43 +18,46 @@ exports.deleteEvents = async (req, res, next) => {
       }
       catch(err) {
         console.log(err)
-        res.status(500).json({ message: err });
+        res.status(500);
+        res.json({ message: err });
       }
   }
 exports.postEvent = async (req, res, next) => {
     try {
         const { firstName, lastName, email, date } = req.body;
-        const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        const correctEmail = email.match(emailPattern);
-        let errorMessage;
-        if(!firstName) {
-          errorMessage = 'firstname';
-        } else if(!lastName) {
-          errorMessage = 'last name';
-        } else if(!email) {
-          errorMessage = 'email';
-        } else if(!correctEmail) {
-          errorMessage = 'valid email';
-        } else if(!date) {
-          errorMessage = 'date';
-        } 
-        if(errorMessage) {
-          res.status(500).json({ message: `Please provide a/an ${errorMessage}` });
-        }
         await Event.create({ firstName, lastName, email, date });
-        res.status(200).json({ message: 'OK' });
+        res.status(200);
+        res.json({ message: 'OK' });
       } catch(err) {
-        console.log(err)
         let errorMessage;
         if(err.errors?.firstName) {
-          errorMessage = 'You have provided incorrect type for a first name. Expected: String';
+          if(err.errors?.firstName?.kind === 'required') {
+            errorMessage = 'Please provide a firstname';
+          } else if(err.errors?.firstName?.valueType !== 'string') {
+            errorMessage = 'You have provided incorrect type for a first name. Expected: String';
+          }
         } else if(err.errors?.lastName) {
-          errorMessage = 'You have provided incorrect type for a last name. Expected: String';
+            if(err.errors?.lastName?.kind === 'required') {
+              errorMessage = 'Please provide a lastname';
+            } else if(err.errors?.lastName?.valueType !== 'string') {
+              errorMessage = 'You have provided incorrect type for a last name. Expected: String';
+            }
         } else if(err.errors?.email) {
-          errorMessage = 'You have provided incorrect type for an email. Expected: String';
+          if(err.errors?.email?.kind === 'required') {
+            errorMessage = 'Please provide an email';
+          } else if(err.errors?.email?.kind === 'regexp') {
+            errorMessage = 'Please provide a valid email';
+          } else if(err.errors?.email?.valueType !== 'string') {
+            errorMessage = 'You have provided incorrect type for a email. Expected: String';
+          }
         } else if(err.errors?.date) {
-          errorMessage = 'You have provided incorrect type for a date. Expected: Date';
+          if(err.errors?.date?.kind === 'required') {
+            errorMessage = 'Please provide a lastname';
+          } else if(err.errors?.date?.valueType !== 'date') {
+            errorMessage = 'You have provided incorrect type for a date. Expected: Date';
+          }
         }
-        res.status(500).json({ message: errorMessage });
+        res.status(500);
+        res.json({ message: errorMessage || 'Network error'});
       }
 }
