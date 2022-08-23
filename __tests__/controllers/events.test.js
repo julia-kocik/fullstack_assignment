fbbb94251648c6822a3db9639e05e5c771978883
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-08-23 19:21:51 +0200

    Add backend test package

--- __tests__/controllers/events.test.js
+++ __tests__/controllers/events.test.js
@@ -1,23 +1,116 @@
 const { postEvent } = require("../../controllers/events");
 const Event = require('../../models/Event');
 
-jest.mock('../../models/Event');
 
-const request = {
-    body: {
-        firstName: 'firstname',
-        lastName: 'lastName', 
-        email: 'email',
-        date: '1994-05-23'
-    }
-}
+
+jest.mock('../../models/Event');
 
 const response = {
     status: jest.fn((x) => x),
     json: jest.fn((x) => x),
 }
 
-it('should send a status code of 400 when user exists', async () => {
-    await postEvent(request, response);
-    expect(response.status).toHaveBeenCalledWith(500);
+const requestOK = {
+    body: {
+        firstName: 'firstname',
+        lastName: 'lastName', 
+        email: 'julia@gmail.com',
+        date: '1994-05-23'
+    }
+}
+
+
+it('should send a status code 200 and correct message when valid data provided', async () => {
+    await postEvent(requestOK, response);
+    expect(response.status).toHaveBeenCalledWith(200);
+    expect(response.json).toHaveBeenCalledWith({"message": "OK"});
 })
+
+const requestWithMissingFirstName = {
+    body: {
+        firstName: false,
+        lastName: 'lastName', 
+        email: 'email',
+        date: '1994-05-23'
+    }
+}
+
+it('should send a status code 500 and message: Please provide a firstname', async () => {
+    await postEvent(requestWithMissingFirstName, response);
+    expect(requestWithMissingFirstName.body.firstName).toBeFalsy();
+    expect(response.status).toHaveBeenCalledWith(500);
+    expect(response.json).toHaveBeenCalledWith({
+        "message": "Please provide firstname"
+    });
+})
+
+const requestWithMissingLastName = {
+    body: {
+        firstName: 'firstName',
+        lastName: '', 
+        email: 'email',
+        date: '1994-05-23'
+    }
+}
+
+it('should send a status code 500 and message: Please provide a lastName', async () => {
+    await postEvent(requestWithMissingLastName, response);
+    expect(requestWithMissingLastName.body.lastName).toBeFalsy();
+    expect(response.status).toHaveBeenCalledWith(500);
+    expect(response.json).toHaveBeenCalledWith({
+        "message": "Please provide lastname"
+    });
+})
+
+const requestWithMissingEmail = {
+    body: {
+        firstName: 'firstName',
+        lastName: 'lastName', 
+        email: '',
+        date: '1994-05-23'
+    }
+}
+
+it('should send a status code 500 and message: Please provide an email', async () => {
+    await postEvent(requestWithMissingEmail, response);
+    expect(requestWithMissingEmail.body.email).toBeFalsy();
+    expect(response.status).toHaveBeenCalledWith(500);
+    expect(response.json).toHaveBeenCalledWith({
+        "message": "Please provide email"
+    });
+})
+
+const requestWithInvalidEmail = {
+    body: {
+        firstName: 'firstName',
+        lastName: 'lastName', 
+        email: 'julia',
+        date: '1994-05-23'
+    }
+}
+
+it('should send a status code 500 and message: Please provide a VALID email', async () => {
+    await postEvent(requestWithInvalidEmail, response);
+    expect(response.status).toHaveBeenCalledWith(500);
+    expect(response.json).toHaveBeenCalledWith({
+        "message": "Please provide valid email"
+    });
+})
+
+const requestWithMissingDate = {
+    body: {
+        firstName: 'firstName',
+        lastName: 'lastName', 
+        email: 'julia@gmail.com',
+        date: 0
+    }
+}
+
+it('should send a status code 500 and message: Please provide a date', async () => {
+    await postEvent(requestWithMissingDate, response);
+    expect(response.status).toHaveBeenCalledWith(500);
+    expect(response.json).toHaveBeenCalledWith({
+        "message": "Please provide date"
+    });
+})
+
--- controllers/events.js
+++ controllers/events.js
@@ -22,12 +22,34 @@ exports.deleteEvents = async (req, res, next) => {
         res.json({ message: err });
       }
   }
+
+//We can validate request data or catch database validation error 
+//Here implemented both options
 exports.postEvent = async (req, res, next) => {
     try {
         const { firstName, lastName, email, date } = req.body;
-        await Event.create({ firstName, lastName, email, date });
-        res.status(200);
-        res.json({ message: 'OK' });
+        const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        const correctEmail = email.match(emailPattern);
+        if(firstName && lastName && email && correctEmail && date) {
+          await Event.create({ firstName, lastName, email, date });
+          res.status(200);
+          res.json({ message: 'OK' });
+        } else {
+          let errorMessage;
+          if(!firstName) {
+            errorMessage = 'Please provide firstname'
+          } else if(!lastName) {
+            errorMessage = 'Please provide lastname'
+          } else if(!email) {
+            errorMessage = 'Please provide email'
+          } else if(!correctEmail) {
+            errorMessage = 'Please provide valid email'
+          } else if(!date) {
+            errorMessage = 'Please provide date'
+          }
+          res.status(500);
+          res.json({ message: errorMessage})
+        }
       } catch(err) {
         let errorMessage;
         if(err.errors?.firstName) {
@@ -52,7 +74,7 @@ exports.postEvent = async (req, res, next) => {
           }
         } else if(err.errors?.date) {
           if(err.errors?.date?.kind === 'required') {
-            errorMessage = 'Please provide a lastname';
+            errorMessage = 'Please provide a date';
           } else if(err.errors?.date?.valueType !== 'date') {
             errorMessage = 'You have provided incorrect type for a date. Expected: Date';
           }
